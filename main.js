d8b898238c0a69ac6f06dcfaa925f614f18ec7d2
Kartik Chawda <chawdakartik@gmail.com>
2021-11-03 00:01:04 +0530

    feat: navigation bar setup

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const toggleButton = document.getElementById('toggleButton');
-const naviList = document.getElementById('naviList');
-toggleButton.addEventListener('click', () => {
-  naviList.classList.toggle('active');
-})
+const toggleButton = document.getElementById("toggleButton");
+console.log(toggleButton);
+const naviList = document.getElementById("naviList");
+toggleButton.addEventListener("click", () => {
+  console.log("Hello");
+  naviList.classList.toggle("active");
+});
 
-const listItem = document.getElementsByClassName('list-item');
+const listItem = document.getElementsByClassName("list-item");
 for (let index = 0; index < listItem.length; index++) {
-  listItem.item(index).addEventListener('click', () => {
-    naviList.className.toggle('deactive')
-  })
-
+  listItem.item(index).addEventListener("click", () => {
+    naviList.className.toggle("deactive");
+  });
 }

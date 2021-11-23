2366476eb3e169adef9560fd4503cf305996b38c
Kartik Chawda <chawdakartik@gmail.com>
2021-11-24 00:01:55 +0530

    feat: navigation bar fix

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,35 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
+// const listClick = document.querySelectorAll(
+//   ".main-list-class.tc-list",
+//   ".socialsClass"
+// );
+
+const mainList = document.querySelectorAll(".main-list-class");
+const tcList = document.querySelectorAll(".tc-list");
+const socialClass = document.querySelectorAll(".socialsClass");
+
+const addListenerForLists = (listClick) => {
+  listClick.forEach((element) => {
+    console.log(element);
+    element.addEventListener("click", () => {
+      const visiblity = nav.getAttribute("data-visible");
+      if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+      } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+      }
+    });
+  });
+};
+
+addListenerForLists(mainList);
+addListenerForLists(tcList);
+addListenerForLists(socialClass);
+
 navToggle.addEventListener("click", () => {
   const visiblity = nav.getAttribute("data-visible");
   if (visiblity === "false") {
